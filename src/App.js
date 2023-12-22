d61b98aa6a009e5a96478db50f9a8c19aabe4055
AerrowDev <sgaeprellegera@gmail.com>
2023-12-22 15:50:53 +0800

    added content

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import {
 } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
-import '../src/styles/header.css'
-import '../src/styles/home.css'
-import '../src/styles/footer.css'
-import '../src/styles/fixed.css'
+import "../src/styles/header.css";
+import "../src/styles/home.css";
+import "../src/styles/footer.css";
+import "../src/styles/fixed.css";
 import WcMarvelbet from "./pages/Content/WcMarvelbet";
 import ArticleMain from "./pages/Content/ArticleMain";
 import CrickexBettingApp from "./pages/Content/CrickexBettingApp";
 import IrelandUltimate from "./pages/Content/IrelandUltimate";
-
+import Australia from "./pages/Content/Australia";
 
 function App() {
   return (
@@ -23,10 +23,23 @@ function App() {
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
-            <Route path="/article/" element={<ArticleMain/>} />
-            <Route path="/article/welcome-to-marvelbet" element={<WcMarvelbet/>} />
-            <Route path="/article/mobile-criket-betting" element={<CrickexBettingApp/>} />
-            <Route path="/article/ireland-ultimate-test" element={<IrelandUltimate/>} />
+            <Route path="/article/" element={<ArticleMain />} />
+            <Route
+              path="/article/welcome-to-marvelbet"
+              element={<WcMarvelbet />}
+            />
+            <Route
+              path="/article/mobile-criket-betting"
+              element={<CrickexBettingApp />}
+            />
+            <Route
+              path="/article/ireland-ultimate-test"
+              element={<IrelandUltimate />}
+            />
+            <Route
+              path="/article/Australia's-Top-5-Century-Scorers-in-ODI-Cricket"
+              element={<Australia />}
+            />
             {/* <Route path="/article/welcome-to-marbelbet" element={<MarvelBet/>} />
             <Route path="/article/welcome-to-marbelbet/:id" element={<ArticlePage/>} /> */}
           </Route>
--- src/pages/Content/ArticleContent.js
+++ src/pages/Content/ArticleContent.js
@@ -1,36 +1,48 @@
 export const main_articles = [
-    {
-        id: 1,
-        slug: 'welcome-to-marvelbet',
-        img: '/assets/hero7.jpg',
-        sub_title: "Your Ultimate Destination for Thrilling Sports Betting and Live Casino Action in India",
-        full_title: "",
-        date: "October 26, 2023",
-        content: [
-            "Welcome to MarvelBet, your one-stop destination for a thrilling and unique online betting and gaming experience in India. At MarvelBet, we pride ourselves on being India's premier platform for live cricket betting and live casino games.",           
-        ]
-    },
-    {
-        id: 2,
-        slug: 'mobile-criket-betting',
-        img: '/assets/article.jpeg',
-        sub_title: "This cricket betting app is getting men all over Asia excited, and its name is Baji999!",
-        full_title: "",
-        date: "October 27, 2023",
-        content: [
-            "The cricket betting trend is sweeping the world, and countless cricket betting players are looking for the best online casino because they want better promotions, gaming experience and other conditions. Now, Baji999 will tell readers how to judge an online casino app, which is worth downloading to your mobile phone.",
-        ]
-    },
-    {
-        id: 3,
-        slug: 'ireland-ultimate-test',
-        img: '/assets/blog-img.jpeg',
-        sub_title: "Ireland's Ultimate Test: Can They Upset India in the 3rd T20",
-        full_title: "",
-        date: "October 28, 2023",
-        content: [
-            "  In the exhilarating world of cricket, every match becomes a canvas for the creation of sporting legends. The clash between Ireland and India in the 3rd T20 promises nothing short of a spectacle that cricket enthusiasts worldwide are eagerly awaiting. With Andy Balbirnie&apos;s resilient 51-ball 72,..",
-        ]
-    },
-    
- ]
+  {
+    id: 1,
+    slug: "welcome-to-marvelbet",
+    img: "/assets/hero7.jpg",
+    sub_title:
+      "Your Ultimate Destination for Thrilling Sports Betting and Live Casino Action in India",
+    full_title: "",
+    date: "October 26, 2023",
+    content: [
+      "Welcome to MarvelBet, your one-stop destination for a thrilling and unique online betting and gaming experience in India. At MarvelBet, we pride ourselves on being India's premier platform for live cricket betting and live casino games.",
+    ],
+  },
+  {
+    id: 2,
+    slug: "mobile-criket-betting",
+    img: "/assets/article.jpeg",
+    sub_title:
+      "This cricket betting app is getting men all over Asia excited, and its name is Baji999!",
+    full_title: "",
+    date: "October 27, 2023",
+    content: [
+      "The cricket betting trend is sweeping the world, and countless cricket betting players are looking for the best online casino because they want better promotions, gaming experience and other conditions. Now, Baji999 will tell readers how to judge an online casino app, which is worth downloading to your mobile phone.",
+    ],
+  },
+  {
+    id: 3,
+    slug: "ireland-ultimate-test",
+    img: "/assets/blog-img.jpeg",
+    sub_title: "Ireland's Ultimate Test: Can They Upset India in the 3rd T20",
+    full_title: "",
+    date: "October 28, 2023",
+    content: [
+      "  In the exhilarating world of cricket, every match becomes a canvas for the creation of sporting legends. The clash between Ireland and India in the 3rd T20 promises nothing short of a spectacle that cricket enthusiasts worldwide are eagerly awaiting. With Andy Balbirnie&apos;s resilient 51-ball 72,..",
+    ],
+  },
+  {
+    id: 4,
+    slug: "Australia's-Top-5-Century-Scorers-in-ODI-Cricket ",
+    img: "/assets/australias-top-5-century-scorers-in-odi-cricket-658537048474d.webp",
+    sub_title: "Australia's Top 5 Century Scorers in ODI Cricket",
+    full_title: "",
+    date: "September 11, 2023",
+    content: [
+      "One of the most exciting aspects of One Day International (ODI) cricket is watching batsmen rack up centuries, displaying their skill, composure, and consistency in the limited-overs format. Australia, a cricketing powerhouse... ",
+    ],
+  },
+];
