7020607578b8500b09d9c388fbce4414ee5186b4
AerrowDev <sgaeprellegera@gmail.com>
2023-12-22 17:17:55 +0800

    added

--- src/pages/Content/WcMarvelbet.jsx
+++ src/pages/Content/WcMarvelbet.jsx
@@ -1,132 +1,204 @@
 import BreadCrumbs from "./BreadCrumbs";
 import "../../styles/about.css";
+import { Helmet } from "react-helmet";
 
 const WcMarvelbet = () => {
   return (
-    <section class="text-white body-font">
-      <div className="container content_container">
-        <BreadCrumbs />
+    <>
+      <Helmet>
+        <title>
+          Your Ultimate Destination for Thrilling Sports Betting and Live Casino
+          Action in India
+        </title>
+        <meta
+          data-react-helmet="true"
+          name="description"
+          content="Your Ultimate Destination for Thrilling Sports Betting and Live Casino Action in India"
+        ></meta>
+        <meta
+          data-react-helmet="true"
+          property="og:title"
+          content="Your Ultimate Destination for Thrilling Sports Betting and Live Casino Action in India"
+        ></meta>
+        <meta
+          data-react-helmet="true"
+          property="og:description"
+          content="Your Ultimate Destination for Thrilling Sports Betting and Live Casino Action in India"
+        ></meta>
+        <meta
+          data-react-helmet="true"
+          property="og:type"
+          content="website"
+        ></meta>
+        <meta data-react-helmet="true" name="og:locale" content="en-BD"></meta>
+        <meta
+          data-react-helmet="true"
+          name="og:image"
+          content="https://www.marvel365bet.com/static/1-1152b446125e6fe1cdfa678efa67abd4.png"
+        ></meta>
+        <meta
+          data-react-helmet="true"
+          property="og:url"
+          content="https://marvel365bet.com"
+        ></meta>
+        <meta
+          data-react-helmet="true"
+          property="og:site_name"
+          content="MarvelBet Online Casino Betting Site in India | Your Ultimate Destination for Thrilling Sports Betting and Live Casino Action in India"
+        ></meta>
+        <meta
+          data-react-helmet="true"
+          property="og:description"
+          content="A recommended online casino platform, what are the major features it should have? MarvelBet, the very popular gaming entertainment brand in India, will tell you. The key to winning the unanimous respect of Indian sports players and gaining the loyalty of their players is to have stable network connection quality, generous promotions of popular benefits, and fair and impartial game products."
+        ></meta>
+        <meta
+          data-react-helmet="true"
+          name="twitter:card"
+          content="summary"
+        ></meta>
+        <meta
+          data-react-helmet="true"
+          name="twitter:creator"
+          content="casinomcw"
+        ></meta>
+        <meta
+          data-react-helmet="true"
+          name="twitter:title"
+          content="Your Ultimate Destination for Thrilling Sports Betting and Live Casino Action in India"
+        ></meta>
+        <meta
+          data-react-helmet="true"
+          name="twitter:description"
+          content="Your Ultimate Destination for Thrilling Sports Betting and Live Casino Action in India"
+        ></meta>
+      </Helmet>
+      <section class="text-white body-font">
+        <div className="container content_container">
+          <BreadCrumbs />
 
-        <div className="article-content">
-          <h2 className="text-2xl text-center pb-2">Welcome to MarvelBet</h2>
-          <h1 className="text-4xl text-center font-semibold pb-3">
-            Your Ultimate Destination for Thrilling Sports Betting and Live
-            Casino Action in India
-          </h1>
-          {/* <img
+          <div className="article-content">
+            <h2 className="text-2xl text-center pb-2">Welcome to MarvelBet</h2>
+            <h1 className="text-4xl text-center font-semibold pb-3">
+              Your Ultimate Destination for Thrilling Sports Betting and Live
+              Casino Action in India
+            </h1>
+            {/* <img
             className="mb-2 w-[100%] md:h-[400px] object-contain"
             alt="casino"
             src="/assets/hero7.jpg"
           /> */}
-          <p className="text-center py-3">
-            Welcome to MarvelBet, your one-stop destination for a thrilling and
-            unique online betting and gaming experience in India. At MarvelBet,
-            we pride ourselves on being India's premier platform for live
-            cricket betting and live casino games. Whether you're a sports
-            enthusiast or a casino gaming aficionado, MarvelBet offers an
-            unparalleled range of betting options and casino games that
-            guarantee endless excitement and the chance to win big.
-          </p>
-          {/* <h3 className="text-3xl pt-3">
+            <p className="text-center py-3">
+              Welcome to MarvelBet, your one-stop destination for a thrilling
+              and unique online betting and gaming experience in India. At
+              MarvelBet, we pride ourselves on being India's premier platform
+              for live cricket betting and live casino games. Whether you're a
+              sports enthusiast or a casino gaming aficionado, MarvelBet offers
+              an unparalleled range of betting options and casino games that
+              guarantee endless excitement and the chance to win big.
+            </p>
+            {/* <h3 className="text-3xl pt-3">
             Elevate Your Cricket Experience with MarvelBet's Live Betting on
             Major Events
           </h3> */}
-          <p className="py-3">
-            MarvelBet understands the passion of Cricket and offers you the
-            opportunity to elevate your cricket experience by placing bets on
-            live matches. Our platform covers all major cricket events,
-            including IPL, international tournaments, and domestic leagues. You
-            can enjoy live betting with competitive odds and a wide array of
-            betting options that cater to both beginners and seasoned bettor
-          </p>
-          {/* 
+            <p className="py-3">
+              MarvelBet understands the passion of Cricket and offers you the
+              opportunity to elevate your cricket experience by placing bets on
+              live matches. Our platform covers all major cricket events,
+              including IPL, international tournaments, and domestic leagues.
+              You can enjoy live betting with competitive odds and a wide array
+              of betting options that cater to both beginners and seasoned
+              bettor
+            </p>
+            {/* 
           <h3 className="text-3xl pt-3">
             Explore Diverse Sporting Opportunities with MarvelBet's
             Comprehensive Sportsbook
           </h3> */}
-          <p className="py-3">
-            MarvelBet doesn't stop at cricket. We offer a comprehensive
-            sportsbook that covers a wide range of sports, including tennis,
-            basketball, football, badminton, kabaddi, and many more. Whether
-            you're a fan of traditional sports or looking to explore new
-            avenues, our sportsbook provides a platform to test your sports
-            knowledge and win big with attractive odds.
-          </p>
-          {/* <h3 className="text-3xl pt-3">
+            <p className="py-3">
+              MarvelBet doesn't stop at cricket. We offer a comprehensive
+              sportsbook that covers a wide range of sports, including tennis,
+              basketball, football, badminton, kabaddi, and many more. Whether
+              you're a fan of traditional sports or looking to explore new
+              avenues, our sportsbook provides a platform to test your sports
+              knowledge and win big with attractive odds.
+            </p>
+            {/* <h3 className="text-3xl pt-3">
             Maximize Your Winnings with MarvelBet's Competitive Odds and
             User-Friendly Interface
           </h3> */}
-          <p className="py-3">
-            We know the role of competitive odds in sports betting. At
-            MarvelBet, we work tirelessly to ensure that our users get the best
-            possible odds, maximizing their potential winnings. With our
-            user-friendly interface and real-time odds updates, you can make
-            informed decisions and enhance your sports betting experience.
-          </p>
-          {/* <h3 className="text-3xl pt-3">
+            <p className="py-3">
+              We know the role of competitive odds in sports betting. At
+              MarvelBet, we work tirelessly to ensure that our users get the
+              best possible odds, maximizing their potential winnings. With our
+              user-friendly interface and real-time odds updates, you can make
+              informed decisions and enhance your sports betting experience.
+            </p>
+            {/* <h3 className="text-3xl pt-3">
             Immerse Yourself in MarvelBet's Cutting-Edge Live Casino Experience
           </h3> */}
-          <p className="py-3">
-            If casino games are your passion, MarvelBet offers a
-            state-of-the-art live casino experience. Enjoy a wide variety of
-            classic and modern casino games, including slots, arcade games,
-            fishing, table games, and lotteries, all brought to life with
-            stunning graphics and seamless gameplay. Our live casino features
-            professional dealers and real-time interaction, providing the most
-            authentic casino experience from the comfort of your home.
-          </p>
-          {/* <h3 className="text-3xl pt-3">
+            <p className="py-3">
+              If casino games are your passion, MarvelBet offers a
+              state-of-the-art live casino experience. Enjoy a wide variety of
+              classic and modern casino games, including slots, arcade games,
+              fishing, table games, and lotteries, all brought to life with
+              stunning graphics and seamless gameplay. Our live casino features
+              professional dealers and real-time interaction, providing the most
+              authentic casino experience from the comfort of your home.
+            </p>
+            {/* <h3 className="text-3xl pt-3">
             Your Security is Our Priority: MarvelBet's Cutting-Edge Data
             Encryption
           </h3> */}
-          <p className="py-3">
-            At MarvelBet, we prioritize the safety and security of our users. We
-            employ the latest encryption technology to safeguard your personal
-            and financial information, ensuring a worry-free gaming experience.
-            Rest assured that your data is in safe hands, allowing you to focus
-            on the excitement of betting and gaming.
-          </p>
-          {/* <h3 className="text-3xl pt-3">
+            <p className="py-3">
+              At MarvelBet, we prioritize the safety and security of our users.
+              We employ the latest encryption technology to safeguard your
+              personal and financial information, ensuring a worry-free gaming
+              experience. Rest assured that your data is in safe hands, allowing
+              you to focus on the excitement of betting and gaming.
+            </p>
+            {/* <h3 className="text-3xl pt-3">
             Around-the-Clock Support: MarvelBet's Reliable Customer Assistance
           </h3> */}
-          <p className="py-3">
-            We know that worries and queries can arise at any time. That's why
-            MarvelBet offers 24/7 customer support to assist you with any
-            queries or issues you may encounter. Our dedicated support team is
-            always ready to provide prompt and reliable assistance, ensuring a
-            smooth and enjoyable experience for all our users.
-          </p>
-          {/* <h3 className="text-3xl pt-3">
+            <p className="py-3">
+              We know that worries and queries can arise at any time. That's why
+              MarvelBet offers 24/7 customer support to assist you with any
+              queries or issues you may encounter. Our dedicated support team is
+              always ready to provide prompt and reliable assistance, ensuring a
+              smooth and enjoyable experience for all our users.
+            </p>
+            {/* <h3 className="text-3xl pt-3">
             Join MarvelBet for an Exciting Adventure in Cricket Betting and
             Casino Gaming
           </h3> */}
-          <p className="py-3">
-            MarvelBet invites you to embark on an exhilarating journey into the
-            world of live cricket betting and casino gaming. Whether you're a
-            seasoned bettor or new to the world of online gaming, our platform
-            caters to all levels of experience. With competitive odds, a diverse
-            range of sports, and a thrilling live casino, MarvelBet is your
-            ticket to winning big and having an unforgettable gaming experience.
-          </p>
-          <p></p>
-          {/* <h3 className="text-3xl pt-3">
+            <p className="py-3">
+              MarvelBet invites you to embark on an exhilarating journey into
+              the world of live cricket betting and casino gaming. Whether
+              you're a seasoned bettor or new to the world of online gaming, our
+              platform caters to all levels of experience. With competitive
+              odds, a diverse range of sports, and a thrilling live casino,
+              MarvelBet is your ticket to winning big and having an
+              unforgettable gaming experience.
+            </p>
+            <p></p>
+            {/* <h3 className="text-3xl pt-3">
             24-7 Support: Your Reliable Partner in ICCWIN Gaming
           </h3> */}
-          {/* <p className="py-3">
+            {/* <p className="py-3">
             We acknowledge that you might have inquiries or apprehensions during
             your ICCWIN gaming experience. Our committed customer support squad
             is at your service round the clock, ready to address any concerns or
             questions you may possess. Do not hesitate to get in touch with us,
             and we will be delighted to provide you with assistance and guidance
           </p> */}
-          <p>
-            {" "}
-            Don't miss out on the action. Join MarvelBet today and let the games
-            begin!
-          </p>
+            <p>
+              {" "}
+              Don't miss out on the action. Join MarvelBet today and let the
+              games begin!
+            </p>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
